63b96911154124d534cbf8aa424cbe96c2547b16
diego2606 <diego.velasquez.ore@gmail.com>
2022-09-01 13:21:26 -0500

    Agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,30 @@
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+// form.addEventListener('submit', sumarInputsValue);
+
+// function sumarInputsValue(event) {
+//     event.preventDefault();
+//     const sumaInputs = Number(input1.value) + Number(input2.value);
+//     pResult.innerText = `Resultado: ${sumaInputs}`;
+// }
+
+
+// btn.addEventListener('click', sumarInputsValue);
+
+// function sumarInputsValue() {
+//     const sumaInputs = Number(input1.value) + Number(input2.value);
+//     pResult.innerText = `Resultado: ${sumaInputs}`;
+// }
+
+form.addEventListener("mouseover", enviarFormulario)
+
+function enviarFormulario(e) {
+    e.preventDefault();
+
+    alert("virus")
+}
